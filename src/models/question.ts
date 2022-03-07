ccb1f43ba9b9c9358845302b7e7dbc6c7f081bfb
Gârleanu Alexandru-Ștefan <redmatterfriends@gmail.com>
2022-03-07 22:41:50 +0200

    Update question.ts

--- src/models/question.ts
+++ src/models/question.ts
@@ -1,14 +1,10 @@
 import { Answer } from "./answer";
 
 export class Question {
-    id: String = "";
-    text: String = "";
-    answers: Array<Object> = [];
-    revealed: boolean = false;
-    constructor(id?: String|any, text?: String|any, answers?: Array<Answer>|any, revealed?:boolean|any) {
-        this.id = id === undefined ? "" : id;
-        this.text = text === undefined ? "" : text;
-        this.answers = answers === undefined ? [] : answers;
-        this.revealed = revealed === undefined ? false : revealed;
-    }
+    public constructor(
+      public readonly id: string = '',
+      public text: string = '',
+      public answers: Answer[] = [],
+      public revealed: boolean = false,
+    ) {}
 }
