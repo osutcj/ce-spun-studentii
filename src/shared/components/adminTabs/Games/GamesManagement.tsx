7f4f461c9b363e3cfdc135ca019bc7bf50960e0d
Alex <alexmihoc@yahoo.com>
2022-11-11 22:54:34 +0200

    Remove duplicates

--- src/shared/components/adminTabs/Games/GamesManagement.tsx
+++ src/shared/components/adminTabs/Games/GamesManagement.tsx
@@ -57,14 +57,11 @@ function GamesManagement() {
     {
         let n = arr.length;
     
-        // Put all array elements in a map
         let s = new Set();
         for (let i = 0; i < n; i++) {
             s.add(arr[i]);
         }
-    
-        // If all elements are distinct, size of
-        // set should be same array.
+
         return (s.size == arr.length);
     }
 
