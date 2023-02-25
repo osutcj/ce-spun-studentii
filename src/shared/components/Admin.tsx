896c5f1fa2f4d5515da81bb14151bfa4839a1c1d
Alex Mihoc <alexmihoc@yahoo.com>
2023-02-25 15:00:34 +0200

    userLocalStorage

--- src/shared/components/Admin.tsx
+++ src/shared/components/Admin.tsx
@@ -13,21 +13,30 @@ import Teams from './adminTabs/Teams/TeamsTab';
 import FlashRoundAdmin from './adminTabs/FlashRound/FlashRoundAdmin';
 import GamesManagement from './adminTabs/Games/GamesManagement';
 import convert from '../../helpers/csv-convertor.helper';
+import { useLocalStorage } from '../../hooks/useLocalStorage';
+
+type userType = {
+  email: string;
+  password: string;
+};
 
 const loginEmail = 'osut@osut.ro';
 const loginPassword = 'ce-spun-studentii-23';
 
 const Admin = () => {
   const [loggedIn, setLoggedIn] = useState(false);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const [currentTab, changeCurrentTab] = useState(0);
+  const [user, setUser] = useLocalStorage<userType>('user', {} as userType);
 
   const handleLogin = () => {
-    if (email === loginEmail && password === loginPassword) {
+    if (user.email === loginEmail && user.password === loginPassword) {
       setLoggedIn(true);
     }
   };
+
+  useEffect(() => {
+    handleLogin();
+  }, [user]);
 
   const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
     changeCurrentTab(newValue);
@@ -55,95 +64,97 @@ const Admin = () => {
     );
   }
 
-  if (!loggedIn) {
+  if (loggedIn) {
     return (
-      <Container maxWidth="sm">
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            backgroundColor: 'white',
-            padding: 5,
-          }}
-        >
-          <Avatar alt="OSUT" sx={{ width: 48, height: 48 }} />
-          <Grid container spacing={3}>
-            <Grid item xs={12}>
-              <TextField
-                fullWidth
-                id="standard-basic"
-                label="Email"
-                type={'email'}
-                variant="standard"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                fullWidth
-                id="standard-basic"
-                label="Password"
-                type={'password'}
-                variant="standard"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <Button
-                fullWidth
-                className="login-button"
-                variant="contained"
-                onClick={handleLogin}
-              >
-                Login
-              </Button>
-            </Grid>
-          </Grid>
+      <Box sx={{ width: '100%' }}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Tabs
+            value={currentTab}
+            style={{ backgroundColor: 'white' }}
+            onChange={handleTabChange}
+            aria-label="basic tabs example"
+          >
+            <Tab label="Joc curent" id={'1'} />
+            <Tab label="Adaugare intrebari" id={'2'} />
+            <Tab label="Adaugare raspunsuri" id={'3'} />
+            <Tab label="Modifica echipe" id={'4'} />
+            <Tab label="Runda fulger" id={'5'} />
+            <Tab label="Manageriere jocuri" id={'6'} />
+          </Tabs>
         </Box>
-      </Container>
+        <TabPanel value={currentTab} index={0}>
+          <CurrentGame />
+        </TabPanel>
+        <TabPanel value={currentTab} index={1}>
+          <Questions />
+        </TabPanel>
+        <TabPanel value={currentTab} index={2}>
+          <AddAnswers />
+        </TabPanel>
+        <TabPanel value={currentTab} index={3}>
+          <Teams />
+        </TabPanel>
+        <TabPanel value={currentTab} index={4}>
+          <FlashRoundAdmin />
+        </TabPanel>
+        <TabPanel value={currentTab} index={5}>
+          <GamesManagement />
+        </TabPanel>
+      </Box>
     );
   }
 
   return (
-    <Box sx={{ width: '100%' }}>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs
-          value={currentTab}
-          style={{ backgroundColor: 'white' }}
-          onChange={handleTabChange}
-          aria-label="basic tabs example"
-        >
-          <Tab label="Joc curent" id={'1'} />
-          <Tab label="Adaugare intrebari" id={'2'} />
-          <Tab label="Adaugare raspunsuri" id={'3'} />
-          <Tab label="Modifica echipe" id={'4'} />
-          <Tab label="Runda fulger" id={'5'} />
-          <Tab label="Manageriere jocuri" id={'6'} />
-        </Tabs>
+    <Container maxWidth="sm">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          backgroundColor: 'white',
+          padding: 5,
+        }}
+      >
+        <Avatar alt="OSUT" sx={{ width: 48, height: 48 }} />
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              id="standard-basic"
+              label="Email"
+              type={'email'}
+              variant="standard"
+              onChange={(e) =>
+                setUser((prev) => ({ ...prev, email: e.target.value }))
+              }
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              id="standard-basic"
+              label="Password"
+              type={'password'}
+              variant="standard"
+              onChange={(e) =>
+                setUser((prev) => ({ ...prev, password: e.target.value }))
+              }
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              fullWidth
+              className="login-button"
+              variant="contained"
+              onClick={handleLogin}
+            >
+              Login
+            </Button>
+          </Grid>
+        </Grid>
       </Box>
-      <TabPanel value={currentTab} index={0}>
-        <CurrentGame />
-      </TabPanel>
-      <TabPanel value={currentTab} index={1}>
-        <Questions />
-      </TabPanel>
-      <TabPanel value={currentTab} index={2}>
-        <AddAnswers />
-      </TabPanel>
-      <TabPanel value={currentTab} index={3}>
-        <Teams />
-      </TabPanel>
-      <TabPanel value={currentTab} index={4}>
-        <FlashRoundAdmin />
-      </TabPanel>
-      <TabPanel value={currentTab} index={5}>
-        <GamesManagement />
-      </TabPanel>
-    </Box>
+    </Container>
   );
 };
 
