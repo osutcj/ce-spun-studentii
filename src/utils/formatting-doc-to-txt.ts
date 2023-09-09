0d4b6144b20ebea7e7a26d7b15781463307e95ab
Vlad <durdeuvladioan@gmail.com>
2023-09-09 10:48:01 +0300

    Task done, submitting for QA review.

--- src/utils/formatting-doc-to-txt.ts
+++ src/utils/formatting-doc-to-txt.ts
@@ -17,21 +17,31 @@ var i = 0;
 while (i < lines.length) {
     // Check if this is a question line
     if (lines[i].includes('.')) {
+       
+
         var question = lines[i].trim().replace(',', '');
 
         console.log(`Question detected: ${question}`);
 
         var answers = [];
-        var points = [];
-
+        var points = [];;
         // Loop through the next 5 lines to get the answers and points
         for (var j = i + 1; j < i + MAX_ANSWERS; j++) {
             try {
                 var line = lines[j].trim().replace('—', '').replace('â€”', '').replace('""', '').replace(',', '').replace("   ", '');
-                var [answer, point] = line.split(' ');
-
+                //var [answer, point] = line.split(' ');
+                const parts = line.split(' ');
+                if (parts.length >= 2) {
+                    var answer = parts.slice(0, -1).join(' '); 
+                    var point = parts[parts.length - 1];
+                }
+                else {
+                    var [answer, point] = line.split(' ');
+                }
                 console.log(`Answer: ${answer} Point: ${point}`);
-
+                if(answer == '' && point == undefined) {
+                    break;
+                }
                 answers.push(answer);
                 points.push(parseInt(point)); // Parse the point as a number
             } catch (error) {
@@ -46,7 +56,10 @@ while (i < lines.length) {
             var maxIndex = points.indexOf(Math.max(...points));
             sortedAnswers.push(answers[maxIndex]);
             
-            sortedAnswers.push(points[maxIndex].toString());
+            if (points[maxIndex] !== undefined) {
+                sortedAnswers.push(points[maxIndex].toString());
+            }
+            
             answers.splice(maxIndex, 1);
             points.splice(maxIndex, 1);
         }
@@ -54,18 +67,16 @@ while (i < lines.length) {
         console.log('Answers: ', answers);
         console.log('Sorted Answers: ', sortedAnswers);
 
-        if (question === '') {
+        
             var row = [question, ...sortedAnswers];
+            // Filter out undefined values from the row
+            row = row.filter(value => value !== undefined);
             console.log('Inserting row: ', row);
             outputData.push(row);
-        } else {
-            var row = [question, ...sortedAnswers];
-            console.log('Inserting row: ', row);
-            outputData.push(row);
-        }
+        
 
         // Move to the next question
-        i += MAX_ANSWERS;
+        i += 2;
     } else {
         // Move to the next line
         i++;
