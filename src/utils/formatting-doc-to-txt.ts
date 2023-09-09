a5d2f13949c1c8f577a70d2d118daacbc3f12668
Vlad <durdeuvladioan@gmail.com>
2023-09-09 12:03:04 +0300

    Fixed the PR comments

--- src/utils/formatting-doc-to-txt.ts
+++ src/utils/formatting-doc-to-txt.ts
@@ -5,93 +5,96 @@ const MAX_ANSWERS = 8;
 const inputFilename = 'input.txt';
 const outputFilename = 'output.csv';
 
-// Read the input file and split it into lines
-const lines = fs.readFileSync(inputFilename, 'utf8').split('\n');
+function writeOutputFile(filename, data) {
+    const header = ['question'];
 
-// Create an array to hold the data for the output CSV file
-var outputData = [];
+    for (let i = 0; i < MAX_ANSWERS; i++) {
+        header.push('answer');
+        header.push('points');
+    }
 
-// Initialize the line index
-var i = 0;
+    const csvRows = [header, ...data];
+    const csvContent = csvRows.map(row => row.join(',')).join('\n');
 
-while (i < lines.length) {
-    // Check if this is a question line
-    if (lines[i].includes('.')) {
-       
+    fs.writeFileSync(filename, csvContent, 'utf8');
+}
 
-        var question = lines[i].trim().replace(',', '');
+function readInputFile(file) {
+    return fs.readFileSync(file, 'utf8').split('\n');
+}
 
-        console.log(`Question detected: ${question}`);
+function processLines(lines) {
+    const outputData = [];
+    let i = 0;
 
-        var answers = [];
-        var points = [];;
-        // Loop through the next 5 lines to get the answers and points
-        for (var j = i + 1; j < i + MAX_ANSWERS; j++) {
-            try {
-                var line = lines[j].trim().replace('—', '').replace('â€”', '').replace('""', '').replace(',', '').replace("   ", '');
-                //var [answer, point] = line.split(' ');
-                const parts = line.split(' ');
-                if (parts.length >= 2) {
-                    var answer = parts.slice(0, -1).join(' '); 
-                    var point = parts[parts.length - 1];
-                }
-                else {
-                    var [answer, point] = line.split(' ');
-                }
-                console.log(`Answer: ${answer} Point: ${point}`);
-                if(answer == '' && point == undefined) {
+    while (i < lines.length) {
+        if (lines[i].includes('.')) {
+            const question = lines[i].trim().replace(',', '');
+            console.log(`Question detected: ${question}`);
+
+            const answers = [];
+            const points = [];
+
+            for (let j = i + 1; j < i + MAX_ANSWERS; j++) {
+                try {
+                    let line = lines[j].trim().replace('—', '').replace('â€”', '').replace('""', '').replace(',', '').replace("   ", '');
+                    const parts = line.split(' ');
+
+                    let answer, point;
+                    if (parts.length >= 2) {
+                        answer = parts.slice(0, -1).join(' ');
+                        point = parts[parts.length - 1];
+                    } else {
+                        [answer, point] = line.split(' ');
+                    }
+
+                    console.log(`Answer: ${answer} Point: ${point}`);
+
+                    if (answer === '' && point === undefined) {
+                        break;
+                    }
+
+                    answers.push(answer);
+                    points.push(parseInt(point));
+                } catch (error) {
+                    console.log("Function processLines caught an error: "+ error);
                     break;
                 }
-                answers.push(answer);
-                points.push(parseInt(point)); // Parse the point as a number
-            } catch (error) {
-                break;
             }
-        }
 
-        // Sort answers by points in descending order
-        var sortedAnswers = [];
+            const sortedAnswers = [];
 
-        while (points.length > 0) {
-            var maxIndex = points.indexOf(Math.max(...points));
-            sortedAnswers.push(answers[maxIndex]);
-            
-            if (points[maxIndex] !== undefined) {
-                sortedAnswers.push(points[maxIndex].toString());
+            while (points.length > 0) {
+                const maxIndex = points.indexOf(Math.max(...points));
+                sortedAnswers.push(answers[maxIndex]);
+
+                if (points[maxIndex] !== undefined) {
+                    sortedAnswers.push(points[maxIndex].toString());
+                }
+
+                answers.splice(maxIndex, 1);
+                points.splice(maxIndex, 1);
             }
-            
-            answers.splice(maxIndex, 1);
-            points.splice(maxIndex, 1);
-        }
 
-        console.log('Answers: ', answers);
-        console.log('Sorted Answers: ', sortedAnswers);
-
-        
-            var row = [question, ...sortedAnswers];
-            // Filter out undefined values from the row
+            let row = [question, ...sortedAnswers];
             row = row.filter(value => value !== undefined);
             console.log('Inserting row: ', row);
             outputData.push(row);
-        
 
-        // Move to the next question
-        i += 2;
-    } else {
-        // Move to the next line
-        i++;
+            i += 2;
+        } else {
+            i++;
+        }
     }
+
+    return outputData;
 }
+
+// Read the input file and split it into lines
+const lines = readInputFile(inputFilename);
+
+// Process the lines to get the output data
+const outputData = processLines(lines);
 
 // Write the output data to the CSV file
-var header = ['question'];
-
-for (var i = 0; i < MAX_ANSWERS; i++) {
-    header.push('answer');
-    header.push('points');
-}
-
-var csvRows = [header, ...outputData];
-var csvContent = csvRows.map(row => row.join(',')).join('\n');
-
-fs.writeFileSync(outputFilename, csvContent, 'utf8');
+writeOutputFile(outputFilename, outputData);
