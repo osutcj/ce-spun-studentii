f8dc830d877115ce844e47c157842c538547882a
Alex <alexmihoc@yahoo.com>
2023-09-16 00:17:03 +0300

    feat: integrated git hooks for eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,24 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
   extends: [
     'plugin:react/recommended',
     'airbnb',
     'airbnb-typescript',
-    'plugin:prettier/recommended',
+    'plugin:prettier/recommended'
   ],
+  ignorePatterns: ['lint-staged.config.js'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: 2021,
+    project: './tsconfig.json'
   },
-  plugins: ['react', '@typescript-eslint'],
   rules: {
-    'react/jsx-filename-extension': [
-      1,
-      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
-    ],
-  },
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }]
+  }
 };
