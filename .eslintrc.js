73a35d4667e91ed33abc014c189785ba02fab436
Alex <alexmihoc@yahoo.com>
2023-09-16 00:27:37 +0300

    comment for users to know in the future

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,8 @@ module.exports = {
     es2021: true
   },
   extends: ['plugin:prettier/recommended'],
+  // extends: ['plugin:prettier/recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended']
+  // Normally, we would use the above line, but we are using just prettier for now. We will add the other plugins later.
   ignorePatterns: ['lint-staged.config.js'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
